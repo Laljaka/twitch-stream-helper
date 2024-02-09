d4a14538daeedc37bf882bd9f0044e715f548bc8
Laljaka <lehabreks@gmail.com>
2024-02-09 12:03:32 +0300

    Introduced contain: paint; and changed selector position to absolute

--- renderer.js
+++ renderer.js
@@ -7,7 +7,7 @@ const modules = document.querySelectorAll('.inactive')
 modules.forEach(async (module, key) => {
   module.addEventListener('click', async() => {
     context = module.id
-    document.getElementById('aaa').style.top = `${(-70 * (modules.length - key))}px`
+    document.getElementById('aaa').style.top = `${(70 * key) + 25}px`
   })
 })
 
