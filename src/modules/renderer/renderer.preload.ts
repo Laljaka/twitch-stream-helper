84f1c843e7b59fcbaca086e0c19dcd2ede129683
Laljaka <watashiwakoroshitai@gmail.com>
2024-03-03 03:43:33 +0300

    added threejs support for the renderer module

--- src/modules/renderer/renderer.preload.ts
+++ src/modules/renderer/renderer.preload.ts
@@ -1,5 +1,27 @@
-import { ipcRenderer, contextBridge } from "electron";
+import { ipcRenderer, contextBridge } from "electron"
+import THREE from 'three'
+import { OBJLoader } from 'three/examples/jsm/Addons'
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+const geom = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geom, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+
 
 contextBridge.exposeInMainWorld('rendererApi', {
+    renderer: renderer.domElement,
+    animate: () => animate(),
     toClose: (callback: Function) => ipcRenderer.once('close', () => callback())
 })
--- src/modules/renderer/renderer.ts
+++ src/modules/renderer/renderer.ts
@@ -1,8 +1,14 @@
-
-
 declare const rendererApi: {
+    renderer: Element,
+    animate: () => void
     toClose: (callback: Function) => Electron.IpcRenderer
 }
+
+document.body.appendChild(rendererApi.renderer)
+window.addEventListener('load', () => {
+    rendererApi.animate()
+})
+
 
 rendererApi.toClose(() => {
     window.close()
