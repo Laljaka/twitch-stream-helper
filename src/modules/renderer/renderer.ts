095edd4ce2202dea8736601eb4a57e6690280bde
Laljaka <watashiwakoroshitai@gmail.com>
2024-03-04 15:55:20 +0300

    Worked on the renderer module

--- src/modules/renderer/renderer.ts
+++ src/modules/renderer/renderer.ts
@@ -1,30 +1,59 @@
-import { ipcRenderer } from "electron"
-import * as THREE from 'three'
-import { OBJLoader } from 'three/examples/jsm/Addons'
+import * as THREE from "three";
+import { OBJLoader } from "three/addons";
+const { ipcRenderer } = require('electron')
+
+ipcRenderer.on('close', () => {
+    window.close()
+})
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 const renderer = new THREE.WebGLRenderer({canvas: document.getElementById('3D') as HTMLCanvasElement})
 renderer.setSize(window.innerWidth, window.innerHeight)
 
-const geom = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geom, material );
-scene.add( cube );
+const loader = new OBJLoader()
+const mtlload = new THREE.TextureLoader()
 
-camera.position.z = 5;
+let clock = new THREE.Clock();
+let delta = 0;
+// 30 fps
+let interval = 1 / 20;
 
-function animate() {
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
-}
+camera.position.z = 3;
 
-//window.addEventListener('load', () => {
-    //document.body.appendChild(renderer.domElement)
-animate()
+const light = new THREE.AmbientLight( 0xffffff ); // soft white light
+scene.add( light );
+
+mtlload.load('../../../content/rat_albedo.png', (texture) => {
+    loader.load('../../../content/eeee.obj', (obj) => {
+        obj.traverse((child) => {
+            if (child instanceof THREE.Mesh) child.material.map = texture
+        })
+        scene.add( obj );
+        function animate() {
+            requestAnimationFrame( animate );
+            obj.rotation.x += 0.005
+            obj.rotation.y += 0.005
+            delta += clock.getDelta();
+
+            if (delta  > interval) {
+                renderer.render( scene, camera );
+
+                delta = delta % interval;
+            }
+        }
+        animate()
+    })
+    
+})
+
+
+
+
+
+
+
 //})
 
 
-ipcRenderer.on('close', () => {
-    window.close()
-})
+
