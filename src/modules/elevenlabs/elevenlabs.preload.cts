b06fadd4f934b297debeb1c2864ff02e0620a0cc
Laljaka <watashiwakoroshitai@gmail.com>
2024-03-06 00:44:23 +0300

    started working on elevenlabs integration

--- src/modules/elevenlabs/elevenlabs.preload.cts
+++ src/modules/elevenlabs/elevenlabs.preload.cts
@@ -1,7 +1,10 @@
 const { ipcRenderer, contextBridge } = require('electron/renderer')
 
+const creds = JSON.parse(process.argv.slice(-1)[0]) as ModuleStorage
+
 contextBridge.exposeInMainWorld('elevenlabsApi', {
     toClose: (callback: Function) => {
         ipcRenderer.once('close', (_e, _v) => callback())
-    }
+    },
+    credentials: creds
 })
--- src/modules/elevenlabs/elevenlabs.ts
+++ src/modules/elevenlabs/elevenlabs.ts
@@ -1,2 +1,51 @@
+import { Source } from "three"
+
+declare global {
+    interface Window {
+        elevenlabsApi: {
+            toClose: (callback: Function) => void
+            credentials : ModuleStorage
+        }
+    }
+}
+
+const source = new MediaSource()
+const audio = document.querySelector('audio')!
+audio.src = window.URL.createObjectURL(source)
+const buffer = source.addSourceBuffer('audio/mp3')
+
+const url = 'https://api.elevenlabs.io/v1/text-to-speech/<voice-id>/stream'
+const chunkSize = 1024 as const
+
+
+const headers = {
+    "Accept": "audio/mpeg",
+    "Content-Type": "application/json",
+    "xi-api-key": window.elevenlabsApi.credentials['key'] as string
+}
+
+const data = {
+    "text": "Born and raised in the charming south, I can add a touch of sweet southern hospitality to your audiobooks and podcasts",
+    "model_id": "eleven_monolingual_v1",
+}
+
+fetch(url, {
+    headers: headers,
+    body: JSON.stringify(data)
+}).then((res) => {
+    const reader = res.body!.getReader()
+    function pump(): Promise<any> {
+        return reader.read().then(({done, value}) => {
+            if (done) {
+                return
+            }
+            buffer.appendBuffer(value)
+            return pump()
+        })
+    }
+    return pump()
+})
+
+
 window.elevenlabsApi.toClose(() => window.close())
 
