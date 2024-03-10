506beac232ae15e7f7c105f6ac7894c6765396e1
Laljaka <watashiwakoroshitai@gmail.com>
2024-03-11 01:03:46 +0300

    this should work but no way to test really untill merge... also got some visuals done

--- src/modules/elevenlabs/elevenlabs.preload.cts
+++ src/modules/elevenlabs/elevenlabs.preload.cts
@@ -3,8 +3,11 @@ const { ipcRenderer, contextBridge } = require('electron/renderer')
 const creds = JSON.parse(process.argv.slice(-1)[0]) as ModuleStorage
 
 contextBridge.exposeInMainWorld('elevenlabsApi', {
-    toClose: (callback: Function) => {
+    onClose: (callback: Function) => {
         ipcRenderer.once('close', (_e, _v) => callback())
     },
-    credentials: creds
+    credentials: creds,
+    onTask: (callback: Function) => {
+        ipcRenderer.on('task', (_e, args) => callback(args))
+    }
 })
--- src/modules/elevenlabs/elevenlabs.ts
+++ src/modules/elevenlabs/elevenlabs.ts
@@ -1,11 +1,33 @@
-
-
 declare global {
     interface Window {
         elevenlabsApi: {
-            toClose: (callback: Function) => void
+            onClose: (callback: Function) => void
             credentials : ModuleStorage
+            onTask: (callback: Function) => void
         }
+    }
+}
+
+class TaskQueue {
+    protected list: Array<Function>
+    protected onTask: boolean
+
+    constructor() {
+        this.list = []
+        this.onTask = false
+    }
+
+    protected execute() {
+        const todo = this.list.shift()
+        if (todo) todo(this.execute)
+        else this.onTask = false
+    }
+    
+    public addTask(callback: Function) {
+        this.list.push(callback)
+        if (this.onTask) return
+        this.onTask = true
+        this.execute()
     }
 }
 
@@ -13,10 +35,59 @@ const source = new MediaSource()
 const audio = document.querySelector('audio')!
 audio.src = window.URL.createObjectURL(source)
 
+async function task(callback: Function, args: any) {
+    const arrayBuffer: Array<Uint8Array> = []
+    const response = await fetch(url, {
+        method: 'GET',
+        //headers: headers,
+        //body: JSON.stringify(data)
+    })
+    const reader = response.body!.getReader()
+    const buffer = source.addSourceBuffer('audio/mp4;codecs="mp4a.40.2"')
+    audio.play()
+    imgsrc.src = "../../../content/speaker.svg"
 
+    async function pump(): Promise<any> {
+        let toAdd = arrayBuffer.shift()
+        if (!toAdd) {
+            const {done, value} = await reader.read()
+            if (done) {
+                source.endOfStream()
+                return 
+            }
+            toAdd = value
+        }
+        try {
+            buffer.appendBuffer(toAdd)                
+        } catch (err) {
+            arrayBuffer.push(toAdd)
+            setTimeout(pump, 10000)
+            return
+        }
+        await new Promise<void>((res, rej) => {
+            buffer.onupdate = () => res()
+        })
+        return await pump()         
+        }
+    audio.addEventListener('ended', () => {
+        source.removeSourceBuffer(buffer)
+        imgsrc.src = "../../../content/nospeaker.svg"
+        callback()
+    })
+    return await pump()
+}
+
+const controls = document.getElementById('controls') as HTMLInputElement
+
+const imgsrc = document.querySelector('img')!
+
+controls.addEventListener('input', () => {
+    audio.volume = parseFloat(controls.value) / 100
+})
 
 //const url = "https://rr4---sn-gvnuxaxjvh-c35d.googlevideo.com/videoplayback?expire=1709873140&ei=lEPqZZuBKOihsfIP19GbuAk&ip=191.101.61.250&id=o-AElUYZ05d47GNyvbWpOoyg8SqVlSOrGjacc7SRofq9Sr&itag=140&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&spc=UWF9f8hOQAa7XuGxZMfwBZtAIehgvb_D56lurlXaXia3Isg&vprv=1&svpuc=1&mime=audio%2Fmp4&gir=yes&clen=200588287&dur=12394.277&lmt=1708709558249128&keepalive=yes&fexp=24007246&c=ANDROID&txp=4432434&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cspc%2Cvprv%2Csvpuc%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRgIhAKoJf3Mzq_mHC8utti0P8CrGGDd0hmfNnHwI02q3Ve9tAiEAhzrOSTHqll-jwMAyueZnW2oNsRynhWKMPJHjIte_HB4%3D&redirect_counter=1&rm=sn-n4vlr7l&req_id=354a700a38cea3ee&cms_redirect=yes&ipbypass=yes&mh=7s&mip=90.154.70.195&mm=31&mn=sn-gvnuxaxjvh-c35d&ms=au&mt=1709851272&mv=m&mvi=4&pl=23&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=APTiJQcwRQIhAN9hZYH2B47E58qsElQTPEZbj5STgm9BUhzrySsgHtMgAiBbUh-qRq8iMU7Yrkt1AP2PN8RmV9Qryjo1cyFqBdMOMQ%3D%3D"
-const url = 'https://w3.tuberipper.com/download/mp3?_k=4e860bc09a3ee53ec213797d17a84cac'
+//const url = 'https://w3.tuberipper.com/download/mp3?_k=4e860bc09a3ee53ec213797d17a84cac'
+const url = 'http://localhost:8000/videoplayback.mp4'
 const headers = {
     "Accept": "audio/mpeg",
     "Content-Type": "application/json",
@@ -28,94 +99,18 @@ const data = {
     "model_id": "eleven_multilingual_v2",
 }
 
-const arrayBuffer: Array<Uint8Array> = []
 
-source.addEventListener('sourceopen', () => {
-    console.log('source ready')
-    console.log('fetching')
-    fetch(url, {
-        method: 'GET',
-        //headers: headers,
-        //body: JSON.stringify(data)
-    }).then(async (res) => {
-        //res.json().then((res) => console.log(res))
-        //console.log(await res.text())
-        console.log('getting body')
-        const reader = res.body!.getReader()
-        console.log('running pump')
-        const buffer = source.addSourceBuffer('audio/mpeg')//;codecs="mp4a.40.2"')
-        audio.play()
-        let cold = true
-        let interval
-        //buffer.onupdate = (ev) => {
-        //    console.log(ev.type)
-        //}
-        async function pump(): Promise<any> {
-            console.log('pump')
-            let toAdd = arrayBuffer.shift()
-            if (!toAdd) {
-                const {done, value} = await reader.read()
-                if (done) {
-                    console.log('dead')
-                    //clearInterval(interval)
-                    return
-                }
-                toAdd = value
-            }
-            
-            
-            //arrayBuffer.push(value)
-            if (buffer.buffered.length !== 0) {
-            console.log(buffer.buffered.start(0))
-                console.log(buffer.buffered.end(0))
-            }
-            try {
-                buffer.appendBuffer(toAdd)                
-            } catch (err) {
-                console.log('errored')
-                arrayBuffer.push(toAdd)
-                setTimeout(pump, 10000)
-                return
-            }
-            await new Promise<void>((res, rej) => {
-                buffer.onupdate = () => res()
-            })
 
-            return await pump()
-            //if (cold && buffer.buffered.length === 0) {
-            //    cold = false
-            //    setTimeout(pump, 1000)
-            //} else {
-                
-            //    setTimeout(pump, (buffer.buffered.end(0)*1000 - audio.currentTime*1000) - 1000)
-            //}
-            
-            
-        }
-        /*interval = setInterval(() => {
-            if (arrayBuffer.length !== 0 && buffer.updating === false) {
-                if (buffer.buffered.length === 1) {
-                    console.log(audio.currentTime)
-                    console.log(buffer.buffered.start(0))
-                    console.log(buffer.buffered.end(0))
-                    if ((buffer.buffered.end(0) - buffer.buffered.start(0)) >= 10) {
-                        if (audio.currentTime >= (buffer.buffered.start(0) + 5)) {
-                            buffer.remove(buffer.buffered.start(0), buffer.buffered.end(0) - 4)
-                            console.log("TRIMMED")
-                        return
-                        }
-                    }
-                }
-                buffer.appendBuffer(arrayBuffer.shift()!)
-            }
-        }, 1000)*/
-        return pump()
+const queue = new TaskQueue()
+
+
+
+
+source.addEventListener('sourceopen', async () => {
+    window.elevenlabsApi.onTask((args:any) => {
+        queue.addTask((callback: Function) => task(callback, args))
     })
 })
 
-
-
-
-
-window.elevenlabsApi.toClose(() => window.close())
+window.elevenlabsApi.onClose(() => window.close())
 
