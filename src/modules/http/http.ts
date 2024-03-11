4af1280efc494c492a9c973f2d9fc802f0e0e245
Laljaka <watashiwakoroshitai@gmail.com>
2024-03-11 23:42:35 +0300

    work in progress commit, just saving changes

--- src/modules/http/http.ts
+++ src/modules/http/http.ts
@@ -1,30 +1,55 @@
 const { ipcRenderer } = require('electron/renderer')
-import http from 'http';
+import Http from 'node:http'
+const http: typeof Http = require('node:http')
+import Fs from 'node:fs'
+const fs: typeof Fs = require('node:fs')
 
 const host = "localhost"
 const port = 6969
 
-const requestListener = function (req:any, res:any) {
-    res.writeHead(200);
-    res.end("My first server!");
-};
+const server = http.createServer((req, res) => {
+    if (req.url === '/polls') {
+        if (req.method === 'GET') {
+            fs.readFile(`${process.cwd()}/dist/modules/http/polls.html`, (err, data) => {
+                if (err) {
+                    res.writeHead(404)
+                    res.end(err.message)
+                } else {
+                    res.setHeader("Content-type", "text/html")
+                    res.writeHead(200)
+                    res.end(data)
+                }
+            })
+        } else if (req.method === 'POST') {
+            res.writeHead(200)
+            res.end('emulating data')
+        }
+    } else if (req.url === '/predictions') {
+        if (req.method === "GET") {
+            fs.readFile(`${process.cwd()}/dist/modules/http/predictions.html`, (err, data) => {
+                if (err) {
+                    res.writeHead(404)
+                    res.end(err.message)
+                } else {
+                    res.setHeader("Content-type", "text/html")
+                    res.writeHead(200)
+                    res.end(data)
+                }
+            })            
+        }
+    } else {
+        res.writeHead(404)
+        res.end("Nothing to see here")
+    }
+})
 
-
-/*
 server.listen(port, host, () => {
-    window.bridge.sendToMain(`Server is running on http://${host}:${port}`)
-    //ipcRenderer.send('http-data', `Server is running on http://${host}:${port}`)
-});
+    //window.bridge.sendToMain(`Server is running on http://${host}:${port}`)
+    ipcRenderer.send('stdout', {from: "http", data: `Server is running on http://${host}:${port}`})
+})
 
-
-
- */
 
 ipcRenderer.once('close', () => {
     window.close()
 })
 
-process.parentPort.on('message', (e) => {
-    const processPort = e.ports[0]
-    processPort.postMessage('aaaaaaaaaaaaaaaaaaaa')
-})
