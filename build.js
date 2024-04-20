ea31070366155ee180d37c8339122b295b716f88
Laljaka <watashiwakoroshitai@gmail.com>
2024-04-21 01:04:21 +0300

    even more building

--- build.js
+++ build.js
@@ -4,7 +4,12 @@ import { rebuild } from "@electron/rebuild"
 import fs from "node:fs"
 import { MSICreator } from "electron-wix-msi"
 import pkg from "./package.json" with { type: "json" }
+import path from "node:path"
+import ora from "ora"
 
+
+let delta = Date.now()
+const spin = ora({ spinner: "dots", color: "yellow" }).start("Packaging modules...")
 
 const dirs = fs.readdirSync("./modules", {withFileTypes: true})
     .filter((dir) => dir.isDirectory())
@@ -13,8 +18,8 @@ const dirs = fs.readdirSync("./modules", {withFileTypes: true})
 fs.mkdirSync(`./modules/modules`)
     
 const proms = dirs.reduce((acc, cur) => {
-    const src = `./modules/${cur}/`;
-    const dest = `./modules/modules/${cur}.asar`;
+    const src = path.resolve(`./modules/${cur}/`);
+    const dest = path.resolve(`./modules/modules/${cur}.asar`);
     acc.push(createPackage(src, dest))
     return acc
 }, [])
@@ -24,47 +29,48 @@ const proms = dirs.reduce((acc, cur) => {
 await Promise.all(proms);
 
 
-console.log('generated asar for modules');
-
-let ar
+spin.succeed(`Modules packaged in ${delta = Date.now() - delta}ms`)
+const spin2 = ora({ spinner: "dots", color: "yellow" }).start("Packaging the app...")
+let spinCallback = ora({ spinner: "dots", color: "yellow" })
 
 const bp = await packager({
-    dir: "./",
+    dir: path.resolve("./"),
     asar: true,
     ignore: /modules|build\.js/g,
-    out: './out',
-    afterCopyExtraResources: [(b, e, p, a, c) => {
-        fs.rm(`./modules/modules`, { recursive: true }, (err) => {
-            if (err) c(err)
-            else c()
-        })
-    }],
-    extraResource: `./modules/modules/`,
+    out: path.resolve('./out'),
+    extraResource: path.resolve(`./modules/modules/`),
     overwrite: true,
+    beforeCopy: [(b,e,p,a,c) => {
+        c()
+    }],
     afterCopy: [(b, e, p, a, c) => {
         rebuild({ buildPath: b, electronVersion: e, arch: a })
             .then(() => c())
             .catch((err) => c(err))
     }],
-    afterComplete: [(b, ev, p, a, c) => {
-        ar = a
+    platform: ["darwin", "win32", "linux"],
+    arch: ["x64", "arm64"],
+    afterComplete: [(b,e,p,a,c) => {
         c()
     }]
 })
 
+spin2.succeed(`App packaged in ${delta = Date.now() - delta}ms`)
+
+await fs.promises.rm(`./modules/modules`, { recursive: true })
+
 const msi = new MSICreator({
-    appDirectory: bp[0],
-    outputDirectory: "./out",
+    appDirectory: path.resolve(bp.find((str) => str.includes("win32-x64"))),
+    outputDirectory: path.resolve("./out/wix-installer"),
     exe: pkg.name,
     description: pkg.description,
     version: pkg.version,
     name: pkg.name,
-    // @ts-ignore
-    arch: ar,
+    arch: "x64",
     manufacturer: pkg.author,
     features: { autoLaunch: false, autoUpdate: false },
     ui: { chooseDirectory: true },
-    icon: "./content/test.ico"
+    icon: path.resolve("./content/test.ico")
 })
 
 await msi.create()
